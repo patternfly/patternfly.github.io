e680683ad534081fa24086db570514dd94700b18
patternfly-build <patternfly-build@redhat.com>
2017-02-15 22:07:26 +0000

    Added files from commit #dcb5ea26d987753ba3d98916f0dfcc2399907ffc

--- components/angular-patternfly/src/charts/line/line-chart.directive.js
+++ components/angular-patternfly/src/charts/line/line-chart.directive.js
@@ -61,6 +61,7 @@
            </div>
            <div class="col-md-3">
                  <button ng-click="addDataPoint()">Add Data Point</button>
+                 <button ng-click="resetData()">Reset Data</button>
            </div>
          </div>
        </div>
@@ -99,8 +100,8 @@
        $scope.data = {
          dataAvailable: true,
          xData: dates,
-         yData0: ['Created', 12, 10,10, 62, 17, 10, 15, 13, 17, 10, 12, 10, 10, 12, 17, 16, 15, 13, 17, 10],
-         yData1: ['Deleted', 10, 17, 76,14, 10, 10, 10, 10, 10, 10, 10, 17, 17, 14, 10, 10, 10, 10, 10, 10]
+         yData0: ['Created', 12, 10, 10, 62, 17, 10, 15, 13, 17, 10, 12, 10, 10, 12, 17, 16, 15, 13, 17, 10],
+         yData1: ['Deleted', 10, 17, 76, 14, 10, 10, 10, 10, 10, 10, 10, 17, 17, 14, 10, 10, 10, 10, 10, 10]
        };
 
        $scope.custShowXAxis = false;
@@ -112,122 +113,132 @@
          $scope.data.yData0.push(Math.round(Math.random() * 100));
          $scope.data.yData1.push(Math.round(Math.random() * 100));
        };
+
+       $scope.resetData = function () {
+         $scope.data = {
+           xData: dates,
+           yData0: ['Created', 12, 10, 10, 62],
+           yData1: ['Deleted', 10, 17, 76, 14]
+         };
+       };
      });
    </file>
  </example>
  */
-angular.module('patternfly.charts').directive('pfLineChart', function (pfUtils) {
+(function (patternfly) {
   'use strict';
-  return {
-    restrict: 'A',
-    scope: {
-      config: '=',
-      chartData: '=',
-      showXAxis: '=?',
-      showYAxis: '=?',
-      setAreaChart: '=?'
-    },
-    replace: true,
-    templateUrl: 'charts/line/line-chart.html',
-    controller: ['$scope',
-      function ($scope) {
+  angular.module('patternfly.charts').directive('pfLineChart', function (pfUtils) {
+    return {
+      restrict: 'A',
+      scope: {
+        config: '=',
+        chartData: '=',
+        showXAxis: '=?',
+        showYAxis: '=?',
+        setAreaChart: '=?'
+      },
+      replace: true,
+      templateUrl: 'charts/line/line-chart.html',
+      controller: ['$scope',
+        function ($scope) {
 
-        // Create an ID for the chart based on the chartId in the config if given
-        $scope.lineChartId = 'lineChart';
-        if ($scope.config.chartId) {
-          $scope.lineChartId = $scope.config.chartId + $scope.lineChartId;
-        }
+          // Create an ID for the chart based on the chartId in the config if given
+          $scope.lineChartId = 'lineChart';
+          if ($scope.config.chartId) {
+            $scope.lineChartId = $scope.config.chartId + $scope.lineChartId;
+          }
 
-        /*
-         * Convert the config data to C3 Data
-         */
-        $scope.getLineData = function (chartData) {
-          var lineData  = {
-            type: $scope.setAreaChart ? "area" : "line"
+          /*
+           * Convert the config data to C3 Data
+           */
+          $scope.getLineData = function (chartData) {
+            var lineData  = {
+              type: $scope.setAreaChart ? "area" : "line"
+            };
+
+            if (chartData && chartData.dataAvailable !== false && chartData.xData) {
+              lineData.x = chartData.xData[0];
+              // Convert the chartData dictionary into a C3 columns data arrays
+              lineData.columns = Object.keys (chartData).map (function (key) {
+                return chartData[key];
+              });
+            }
+
+            return lineData;
           };
 
-          if (chartData && chartData.dataAvailable !== false && chartData.xData) {
-            lineData.x = chartData.xData[0];
-            // Convert the chartData dictionary into a C3 columns data arrays
-            lineData.columns = Object.keys (chartData).map (function (key) {
-              return chartData[key];
-            });
+          /*
+           * Setup Axis options. Default is to not show either axis. This can be overridden in two ways:
+           *   1) in the config, setting showAxis to true will show both axes
+           *   2) in the attributes showXAxis and showYAxis will override the config if set
+           *
+           * By default only line and the tick marks are shown, no labels. This is a line and should be used
+           * only to show a brief idea of trending. This can be overridden by setting the config.axis options per C3
+           */
+
+          if ($scope.showXAxis === undefined) {
+            $scope.showXAxis = ($scope.config.showAxis !== undefined) && $scope.config.showAxis;
           }
 
-          return lineData;
-        };
-
-        /*
-         * Setup Axis options. Default is to not show either axis. This can be overridden in two ways:
-         *   1) in the config, setting showAxis to true will show both axes
-         *   2) in the attributes showXAxis and showYAxis will override the config if set
-         *
-         * By default only line and the tick marks are shown, no labels. This is a line and should be used
-         * only to show a brief idea of trending. This can be overridden by setting the config.axis options per C3
-         */
-
-        if ($scope.showXAxis === undefined) {
-          $scope.showXAxis = ($scope.config.showAxis !== undefined) && $scope.config.showAxis;
-        }
-
-        if ($scope.showYAxis === undefined) {
-          $scope.showYAxis = ($scope.config.showAxis !== undefined) && $scope.config.showAxis;
-        }
-
-        $scope.defaultConfig = $().c3ChartDefaults().getDefaultLineConfig();
-        $scope.defaultConfig.axis = {
-          x: {
-            show: $scope.showXAxis === true,
-            type: 'timeseries',
-            tick: {
-              format: function () {
-                return '';
-              }
-            }
-          },
-          y: {
-            show: $scope.showYAxis === true,
-            tick: {
-              format: function () {
-                return '';
-              }
-            }
+          if ($scope.showYAxis === undefined) {
+            $scope.showYAxis = ($scope.config.showAxis !== undefined) && $scope.config.showAxis;
           }
-        };
 
-        /*
-         * Setup Chart type option. Default is Line Chart.
-         */
-        if ($scope.setAreaChart === undefined) {
-          $scope.setAreaChart = ($scope.config.setAreaChart !== undefined) && $scope.config.setAreaChart;
+          $scope.defaultConfig = patternfly.c3ChartDefaults().getDefaultLineConfig();
+          $scope.defaultConfig.axis = {
+            x: {
+              show: $scope.showXAxis === true,
+              type: 'timeseries',
+              tick: {
+                format: function () {
+                  return '';
+                }
+              }
+            },
+            y: {
+              show: $scope.showYAxis === true,
+              tick: {
+                format: function () {
+                  return '';
+                }
+              }
+            }
+          };
+
+          /*
+           * Setup Chart type option. Default is Line Chart.
+           */
+          if ($scope.setAreaChart === undefined) {
+            $scope.setAreaChart = ($scope.config.setAreaChart !== undefined) && $scope.config.setAreaChart;
+          }
+
+          // Convert the given data to C3 chart format
+          $scope.config.data = pfUtils.merge($scope.config.data, $scope.getLineData($scope.chartData));
+
+          // Override defaults with callers specifications
+          $scope.defaultConfig = pfUtils.merge($scope.defaultConfig, $scope.config);
         }
+      ],
 
-        // Convert the given data to C3 chart format
-        $scope.config.data = pfUtils.merge($scope.config.data, $scope.getLineData($scope.chartData));
-
-        // Override defaults with callers specifications
-        $scope.defaultConfig = pfUtils.merge($scope.defaultConfig, $scope.config);
+      link: function (scope) {
+        scope.$watch('config', function () {
+          scope.config.data = pfUtils.merge(scope.config.data, scope.getLineData(scope.chartData));
+          scope.chartConfig = pfUtils.merge(scope.defaultConfig, scope.config);
+        }, true);
+        scope.$watch('showXAxis', function () {
+          scope.chartConfig.axis.x.show = scope.showXAxis === true;
+        });
+        scope.$watch('showYAxis', function () {
+          scope.chartConfig.axis.y.show = scope.showYAxis === true;
+        });
+        scope.$watch('setAreaChart', function () {
+          scope.chartConfig.data.type = scope.setAreaChart ? "area" : "line";
+        });
+        scope.$watch('chartData', function () {
+          scope.chartConfig.data = scope.getLineData(scope.chartData);
+        }, true);
       }
-    ],
+    };
+  });
+}(patternfly));
 
-    link: function (scope) {
-      scope.$watch('config', function () {
-        scope.config.data = pfUtils.merge(scope.config.data, scope.getLineData(scope.chartData));
-        scope.chartConfig = pfUtils.merge(scope.defaultConfig, scope.config);
-      }, true);
-      scope.$watch('showXAxis', function () {
-        scope.chartConfig.axis.x.show = scope.showXAxis === true;
-      });
-      scope.$watch('showYAxis', function () {
-        scope.chartConfig.axis.y.show = scope.showYAxis === true;
-      });
-      scope.$watch('setAreaChart', function () {
-        scope.chartConfig.data.type = scope.setAreaChart ? "area" : "line";
-      });
-      scope.$watch('chartData', function () {
-        scope.chartConfig.data = pfUtils.merge(scope.chartConfig.data, scope.getLineData(scope.chartData));
-      }, true);
-    }
-  };
-}
-);
